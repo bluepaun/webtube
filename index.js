b786eca41babccb47956e490a5908defffb7ac35
bluepaun <bluepaun@gmail.com>
2020-07-27 01:05:33 +0900

    middle ware

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = 4000;
 
+const PORTHTTP = 80;
+
 const handleListening = () => console.log(`Listening ion : http://localhost:${PORT}`);
 
 
@@ -11,9 +13,19 @@ const handleHome = (req, res) => res.send(`Hello from home hehe`);
 
 const handleProfile = (req, res) => res.send(`you are on my profile`);
 
+const betweenHome = (req, res, next) =>{
+    console.log(`between`);
+    next();
+}
 
-app.get("/", handleHome);
+//this function is working sequencely 순차적으로 시작
+app.use(betweenHome);
+
+//app.get("/", betweenHome .....,handleHome);
+app.get("/",handleHome);
 
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
+
+//app.listen(PORTHTTP, handleListening);
