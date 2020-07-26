3e614c96cbdfd27609a757da593dbc3db5ec9eae
bluepaun <bluepaun@gmail.com>
2020-07-26 22:10:05 +0900

    es6 nodejs using babel
    babel is translate new js to old style js
    nodemon is watcher project files for auto restart
    devDependencis is only for developer using module
    
    new syntax for js in ES6

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening(){
-    console.log(`Listening ion : http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening ion : http://localhost:${PORT}`);
 
+
+const handleHome = (req, res) => res.send(`Hello from home hehe`);
+
+
+const handleProfile = (req, res) => res.send(`you are on my profile`);
+
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
