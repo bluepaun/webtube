714401d343b2d89405feca678c48c4a0c4610cb2
bluepaun <bluepaun@gmail.com>
2020-07-27 01:54:25 +0900

    routing
    module ES6
    router
    import
    export (default)

--- app.js
+++ app.js
@@ -3,17 +3,14 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
+
+
+console.log("app.js");
 
 const app = express();
 
-const PORT = 4000;
-const PORTHTTP = 80;
-
-const handleListening = () => console.log(`Listening ion : http://localhost:${PORT}`);
-
-
 const handleHome = (req, res) => res.send(`Hello from home hehe`);
-
 
 const handleProfile = (req, res) => res.send(`you are on my profile`);
 
@@ -24,14 +21,11 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-
-app.use(middleware);
-
 //route
 app.get("/",handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
 
-//app.listen(PORTHTTP, handleListening);
+export default app;
