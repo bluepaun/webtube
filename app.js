6e063dc7b53bea94a65116aced047b734d292f56
bluepaun <bluepaun@gmail.com>
2020-07-27 02:38:08 +0900

    make routes constant
    make routes constant

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import globalRouter from "./routers/globalRouter";
 import userRouter  from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 const app = express();
 
 //middleware
@@ -16,8 +17,8 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 //route
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
