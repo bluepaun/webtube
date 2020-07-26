110beda2d8bc79eb3b3fc64e909cfe5d56fd71e0
bluepaun <bluepaun@gmail.com>
2020-07-27 03:27:00 +0900

    make send rander

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
-export const join = (req, res) => res.send(`join`);
-export const login = (req, res) => res.send(`login`);
-export const logout = (req, res) => res.send(`logout`);
+export const join = (req, res) => res.rander(`join`);
+export const login = (req, res) => res.rander(`login`);
+export const logout = (req, res) => res.rander(`logout`);
 
-export const users = (req, res) => res.send(`users`);
-export const userDetail = (req, res) => res.send(`userDetail`);
-export const editProfile = (req, res) => res.send(`editProfile`);
-export const changePassword = (req, res) => res.send(`changePassword`);
+export const users = (req, res) => res.rander(`users`);
+export const userDetail = (req, res) => res.rander(`userDetail`);
+export const editProfile = (req, res) => res.rander(`editProfile`);
+export const changePassword = (req, res) => res.rander(`changePassword`);
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,8 @@
 export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("search");
+export const search = (req, res) => res.rander("search");
 
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("videoDetail");
-export const editVideo = (req, res) => res.send("editVideo");
-export const deleteVideo = (req, res) => res.send("deleteVideo");
+export const videos = (req, res) => res.rander("videos");
+export const upload = (req, res) => res.rander("upload");
+export const videoDetail = (req, res) => res.rander("videoDetail");
+export const editVideo = (req, res) => res.rander("editVideo");
+export const deleteVideo = (req, res) => res.rander("deleteVideo");
