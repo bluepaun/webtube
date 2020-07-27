165a2004c450fe18d0c4e3ddcfdaffddc18c8bb0
bluepaun <bluepaun@gmail.com>
2020-07-28 02:25:24 +0900

    Template Variables in Pug
    this variables can only be used in specific template file

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,7 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.rander("search");
-
-export const videos = (req, res) => res.rander("videos");
-export const upload = (req, res) => res.rander("upload");
-export const videoDetail = (req, res) => res.rander("videoDetail");
-export const editVideo = (req, res) => res.rander("editVideo");
-export const deleteVideo = (req, res) => res.rander("deleteVideo");
+export const home = (req, res) => res.render("home",  {pageTitle : `Home`});
+export const search = (req, res) => res.rander("search",  {pageTitle : `Search`});
+export const videos = (req, res) => res.rander("videos",  {pageTitle : `Videos`});
+export const upload = (req, res) => res.rander("upload",  {pageTitle : `Upload`});
+export const videoDetail = (req, res) => res.rander("videoDetail",  {pageTitle : `Video Detail`});
+export const editVideo = (req, res) => res.rander("editVideo",  {pageTitle : `Edit Video`});
+export const deleteVideo = (req, res) => res.rander("deleteVideo",  {pageTitle : `Delete Video`});
