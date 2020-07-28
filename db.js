50b8df2ed9e27796b4dcad7895722e9d8f20edbd
bluepaun <bluepaun@gmail.com>
2020-07-29 00:38:56 +0900

    home controller
    in pug
    each item in array
    h1 = item.value

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:2321123,
+        title: 'Video awesome',
+        description: "this is wtf",
+        views:445,
+        videoFile:"mov_bbb.ogg",
+        creator: {
+            id : 1111,
+            name : "cjw",
+            email : "aaa@ffff.com"
+        }
+    },
+    {
+        id:14235,
+        title: 'Video aaaa',
+        description: "this is wtf",
+        views:445,
+        videoFile:"mov_bbb.ogg",
+        creator: {
+            id : 1111,
+            name : "cjw",
+            email : "aaa@ffff.com"
+        }
+    },
+    {
+        id:2321111123,
+        title: 'Video ffff',
+        description: "this is wtf",
+        views:445,
+        videoFile:"mov_bbb.ogg",
+        creator: {
+            id : 1111,
+            name : "cjw",
+            email : "aaa@ffff.com"
+        }
+    },
+    {
+        id:231111111123,
+        title: 'Video ffaaaaff',
+        description: "this is wtf",
+        views:445,
+        videoFile:"mov_bbb.ogg",
+        creator: {
+            id : 1111,
+            name : "cjw",
+            email : "aaa@ffff.com"
+        }
+    }
+]
