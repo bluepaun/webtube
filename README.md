7f6fe013f6296957b71b44f03c06ac3d3e8b7848
bluepaun <bluepaun@gmail.com>
2020-07-28 20:52:47 +0900

    search
    form method get action
    search page

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
  webtube
 
 Cloning Youtube with Vanilia and Nodejs
+
+
+## Pages:
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,10 @@
 export const home = (req, res) => res.render("home",  {pageTitle : `Home`});
-export const search = (req, res) => res.rander("search",  {pageTitle : `Search`});
-export const videos = (req, res) => res.rander("videos",  {pageTitle : `Videos`});
-export const upload = (req, res) => res.rander("upload",  {pageTitle : `Upload`});
-export const videoDetail = (req, res) => res.rander("videoDetail",  {pageTitle : `Video Detail`});
-export const editVideo = (req, res) => res.rander("editVideo",  {pageTitle : `Edit Video`});
-export const deleteVideo = (req, res) => res.rander("deleteVideo",  {pageTitle : `Delete Video`});
+export const search = (req, res) => {
+    const {query: { term : searchingBy}} = req;
+    res.render("search",  {pageTitle : `Search`, searchingBy});
+};
+export const videos = (req, res) => res.render("videos",  {pageTitle : `Videos`});
+export const upload = (req, res) => res.render("upload",  {pageTitle : `Upload`});
+export const videoDetail = (req, res) => res.render("videoDetail",  {pageTitle : `Video Detail`});
+export const editVideo = (req, res) => res.render("editVideo",  {pageTitle : `Edit Video`});
+export const deleteVideo = (req, res) => res.render("deleteVideo",  {pageTitle : `Delete Video`});
