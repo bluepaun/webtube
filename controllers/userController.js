f63ea33ba8302ab6878844ac89ee73f8156d300b
bluepaun <bluepaun@gmail.com>
2020-07-30 00:51:09 +0900

    Log in user profiles video detail upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,31 +2,40 @@ import routes from "../routes";
 
 export const getJoin = (req, res) => {
     console.log(req.body);
-    res.render(`join`,  {pageTitle : `Join`});
+    res.render(`join`, { pageTitle: `Join` });
 };
 
 export const postJoin = (req, res) => {
     const {
-        body: {name, email, password, password2}
+        body: { name, email, password, password2 },
     } = req;
 
-    if( password !== password2){
-        res.status(400);// bad request http status code
-        res.render(`join`,  {pageTitle : `Join`});
+    if (password !== password2) {
+        res.status(400); // bad request http status code
+        res.render(`join`, { pageTitle: `Join` });
     } else {
         //To Do: Register User
         //To Do: Log user in
         res.redirect(routes.home);
     }
-
 };
 
+export const getLogin = (req, res) => {
+    res.render(`login`, { pageTitle: `Login` });
+};
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
 
+export const logout = (req, res) => {
+    //To DO : process Log out
+    res.redirect(routes.home);
+};
 
-export const login = (req, res) => res.render(`login`,  {pageTitle : `Login`});
-export const logout = (req, res) => res.render(`logout`,  {pageTitle : `Logout`});
-
-export const users = (req, res) => res.render(`users`,  {pageTitle : `Users`});
-export const userDetail = (req, res) => res.render(`userDetail`,  {pageTitle : `User Detail`});
-export const editProfile = (req, res) => res.render(`editProfile`,  {pageTitle : `Edit Profile`});
-export const changePassword = (req, res) => res.render(`changePassword`, {pageTitle : `Change Password`});
+export const users = (req, res) => res.render(`users`, { pageTitle: `Users` });
+export const userDetail = (req, res) =>
+    res.render(`userDetail`, { pageTitle: `User Detail` });
+export const editProfile = (req, res) =>
+    res.render(`editProfile`, { pageTitle: `Edit Profile` });
+export const changePassword = (req, res) =>
+    res.render(`changePassword`, { pageTitle: `Change Password` });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,20 @@
 import express from "express";
 import routes from "../routes";
-import { upload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
+import {
+    getUpload,
+    videoDetail,
+    editVideo,
+    deleteVideo,
+    postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
 
-videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
-videoRouter.get(routes.videoDetail, videoDetail);
-
+videoRouter.get(routes.videoDetail(), videoDetail);
 
 export default videoRouter;
