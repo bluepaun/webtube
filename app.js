e38bb50d6675baea52ceb368f419b1069add3cba
bluepaun <bluepaun@gmail.com>
2020-07-29 21:48:49 +0900

    join
    post
    get
    http status code
    body parser

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const app = express();
 app.use(helmet());
 app.set('view engine', "pug");
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());//decoding req can make use req.body
+app.use(bodyParser.urlencoded({extended : true}));//
 app.use(morgan("dev"));
 
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,28 @@
-export const join = (req, res) => res.render(`join`,  {pageTitle : `Join`});
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+    console.log(req.body);
+    res.render(`join`,  {pageTitle : `Join`});
+};
+
+export const postJoin = (req, res) => {
+    const {
+        body: {name, email, password, password2}
+    } = req;
+
+    if( password !== password2){
+        res.status(400);// bad request http status code
+        res.render(`join`,  {pageTitle : `Join`});
+    } else {
+        //To Do: Register User
+        //To Do: Log user in
+        res.redirect(routes.home);
+    }
+
+};
+
+
+
 export const login = (req, res) => res.render(`login`,  {pageTitle : `Login`});
 export const logout = (req, res) => res.render(`logout`,  {pageTitle : `Logout`});
 
