a3af0da6f0c5991b3cb7ba72f7aa13d9bf865493
bluepaun <bluepaun@gmail.com>
2020-07-31 21:02:25 +0900

    connecting mongoDB

--- init.js
+++ init.js
@@ -1,10 +1,12 @@
+import "./db";
 import app from "./app";
 
 console.log("init.js");
 const PORT = 4000;
-const PORT_HTTP=80;
+const PORT_HTTP = 80;
 
-const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
+const handleListening = () =>
+    console.log(`Listening on : http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
 //app.listen(PORT_HTTP, handleListening);
